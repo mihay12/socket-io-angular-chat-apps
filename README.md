214e065270c107df55e96e74e79ecb238ea43c9c
mihay12 <mihailgaravsyuk@gmail.com>
2022-11-03 09:13:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# socket-io-angular-chat-apps
+
+1 - https://deepinder.me/creating-a-real-time-app-with-angular-8-and-socket-io-with-nodejs
+
+2 - https://deepinder.me/creating-a-realtime-chat-app-with-angular-and-socket-io-with-nodejs-part-2
---
+++ index.js
@@ -0,0 +1,30 @@
+// Express ініціалізується в app як обробник функції, який можна надати HTTP-серверу 
+const app = require('express')();
+const http = require('http').createServer(app);
+
+// я ініціалізую новий екземпляр у socket.io,передавши http об’єкт (сервер HTTP) і параметри cors (оновлені для socket.io v3), 
+// щоб дозволити нашу URL-адресу локального хостa angular 
+// (яку ви можете вставити в URL-адресу або свій зовнішній клієнт, у моєму випадку це було. localhost:4200)
+const io = require('socket.io')(http, {
+    cors: {
+        origins: ['http://localhost:4200']
+    }
+});
+
+// Ми визначаємо обробник маршруту, /який викликається, коли ми переходимо на головну сторінку нашого сайту.
+app.get('/', (req, res) => { // в параметрах функції спочатку йде параметр запиту (req), а потім відповідні (res)
+    res.send('<h1>Hey Socket.io</h1>');
+  });
+
+// Прослуховую події connection та disconnection для вхідних сокетів і реєструю це на консолі.
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+})
+
+// Ми змушуємо http-сервер прослуховувати порт 3000.
+http.listen(3000, () => {
+    console.log('listeninghttp on *:3000');
+})
