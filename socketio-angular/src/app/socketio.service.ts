5a98af1bbdca17d3f1c3269ff2fc6b833656b4a7
mihay12 <mihailgaravsyuk@gmail.com>
2022-11-03 11:29:30 +0200

    Make event handling: send message, broadcast message and authentification

--- socketio-angular/src/app/socketio.service.ts
+++ socketio-angular/src/app/socketio.service.ts
@@ -9,14 +9,28 @@ export class SocketioService {
 
   socket: any;
 
-  constructor() { }
+  constructor() {
+   }
 
   // функція ініціалізації сокета
   setupSocketConnection() {
-    this.socket = io(environment.SOCKET_ENDPOINT);
+    this.socket = io(environment.SOCKET_ENDPOINT, {
+      // Надсилання query parameters на бекенд під час підключення сокета
+      auth: {
+        token: 'cde'
+      }
+    });
+
+    // Передача з ангуляру тексту через подію 'my message'
+    this.socket.emit('my message', 'Hello there from Angular');
+
+    // Друк тексту у консолі переданого з серверної сторони
+    this.socket.on('my broadcast', (data: string) => {
+      console.log(data);
+    });
   }
 
-  // функція відключення сокета
+  // Функція відключення сокета
   disconnect() {
     if (this.socket) {
       this.socket.disconnect();
